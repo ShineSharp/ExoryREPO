c619ad78ccd999fd640c70f6bd16d987757a11b9
ExoryREPO <drago.enrico@hotmail.it>
2015-12-12 16:41:47 +0100

    ExorAIO: Ashe - Fix!

--- ExorAIO/AIO/Champions/Ashe/Utilities/Logics.cs
+++ ExorAIO/AIO/Champions/Ashe/Utilities/Logics.cs
@@ -42,7 +42,7 @@ namespace ExorAIO.Champions.Ashe
                 (Bools.IsImmobile(Targets.Target) &&
                     Variables.Menu.Item($"{Variables.MainMenuName}.wsettings.usewimmobile").GetValue<bool>()))
             {
-                Variables.W.Cast(Targets.Target.Position);
+                Variables.W.CastIfHitchanceEquals(Targets.Target, HitChance.High, false);
             }
             
             /// <summary>
