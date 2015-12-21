7c857dd895f0be2d4e5e0f26eaf4911795c2c108
ExoryREPO <drago.enrico@hotmail.it>
2015-12-21 02:42:50 +0100

    ExorLucian: .

--- ExorLucian/Properties/AssemblyInfo.cs
+++ ExorLucian/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExorKalista")]
+[assembly: AssemblyTitle("ExorLucian")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LeagueSharp")]
-[assembly: AssemblyProduct("ExorKalista")]
+[assembly: AssemblyProduct("ExorLucian")]
 [assembly: AssemblyCopyright("Copyright © Exory 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.24.0.0")]
-[assembly: AssemblyFileVersion("5.24.0.0")]
+[assembly: AssemblyVersion("5.24.0.1")]
+[assembly: AssemblyFileVersion("5.24.0.1")]
