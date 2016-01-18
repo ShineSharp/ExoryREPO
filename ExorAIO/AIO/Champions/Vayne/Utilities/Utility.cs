28236da8c7918712b5036884a2d7b1d032292e3c
ExoryREPO <drago.enrico@hotmail.it>
2016-01-18 23:23:34 +0100

    ExorAIO: Vayne - Condemn Prediction test.

--- ExorAIO/AIO/Champions/Vayne/Utilities/Utility.cs
+++ ExorAIO/AIO/Champions/Vayne/Utilities/Utility.cs
@@ -25,8 +25,9 @@ namespace ExorAIO.Champions.Vayne
             Variables.Q = new Spell(SpellSlot.Q, ObjectManager.Player.BoundingRadius + (ObjectManager.Player.AttackRange + 300f));
             Variables.W = new Spell(SpellSlot.W);
             Variables.E = new Spell(SpellSlot.E, ObjectManager.Player.BoundingRadius*2 + 550f);
-            
-            Variables.E.SetTargetted(0.25f, 1250f);
+
+            //Variables.E.SetTargetted(0.25f, 1250f);
+            Variables.E.SetTargetted(0.413f, 1000f);
         }
 
         /// <summary>
