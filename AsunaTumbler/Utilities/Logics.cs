d76cff1f3219a258994a49ead9d4280d7057cd1b
ExoryREPO <drago.enrico@hotmail.it>
2016-01-19 16:26:02 +0100

    AsunaTumbler: Added On-Click WallTumble.

--- AsunaTumbler/Utilities/Logics.cs
+++ AsunaTumbler/Utilities/Logics.cs
@@ -25,14 +25,13 @@ namespace AsunaTumbler
             /// </summary>
             
             //Ty Asuna
-            if (Environment.TickCount - Variables.LastMoveC < 200)
+            if (Environment.TickCount - Variables.LastMoveC < 80)
             {
                 return;
             }
             Variables.LastMoveC = Environment.TickCount;
 
-            if (Variables.Q.IsReady() &&
-                Variables.Menu.Item($"{Variables.MainMenuName}.walltumbler.executewalltumble").GetValue<KeyBind>().Active)
+            if (Variables.Q.IsReady())
             {
                 var selectedPos =
                     ObjectManager.Player.Distance(new Vector2(11514, 4462)) <
@@ -46,13 +45,23 @@ namespace AsunaTumbler
                         new Vector2(12050, 4827) :
                         new Vector2(6962, 8952);
 
-                if (ObjectManager.Player.Distance(walkPos) > 50)
+                if (Variables.Menu.Item($"{Variables.MainMenuName}.walltumbler.executewalltumble").GetValue<KeyBind>().Active)
                 {
-                    ObjectManager.Player.IssueOrder(GameObjectOrder.MoveTo, walkPos.To3D());
-                    return;
+                    if (ObjectManager.Player.Distance(walkPos) > 50)
+                    {
+                        ObjectManager.Player.IssueOrder(GameObjectOrder.MoveTo, walkPos.To3D());
+                        return;
+                    }
                 }
 
-                Variables.Q.Cast(selectedPos.To3D());
+                if (Variables.Menu.Item($"{Variables.MainMenuName}.walltumbler.executewalltumble").GetValue<KeyBind>().Active ||
+                    Variables.Menu.Item($"{Variables.MainMenuName}.walltumbler.enableonclickwalltumble").GetValue<bool>())
+                {
+                    if (ObjectManager.Player.Distance(walkPos) < 10)
+                    {
+                        Variables.Q.Cast(selectedPos.To3D());
+                    }
+                }
             }
         }
     }
