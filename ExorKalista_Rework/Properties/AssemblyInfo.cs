476b4f59f5bc0f414b66611867ae3e55686ac7c5
ExoryREPO <drago.enrico@hotmail.it>
2016-01-20 22:00:30 +0100

    ExorKalista: Rework - Ayy lmao

--- ExorKalista_Rework/Properties/AssemblyInfo.cs
+++ ExorKalista_Rework/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExorKalista")]
+[assembly: AssemblyTitle("ExorKalista_Rework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LeagueSharp")]
-[assembly: AssemblyProduct("ExorKalista")]
+[assembly: AssemblyProduct("ExorKalista_Rework")]
 [assembly: AssemblyCopyright("Copyright © Exory 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
