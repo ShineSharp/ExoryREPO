56dd37489b392dc9c7699c77fc1878a63c696960
ExoryREPO <drago.enrico@hotmail.it>
2016-01-25 23:04:58 +0100

    ExorLucian: Conforming.

--- ExorLucian/Utilities/Properties/Menus.cs
+++ ExorLucian/Utilities/Properties/Menus.cs
@@ -43,7 +43,7 @@ namespace ExorLucian
                 /// <summary>
                 /// Sets the spells menu.
                 /// </summary>
-                Variables.SettingsMenu = new Menu("Spells Menu", $"{Variables.MainMenuName}.settingsmenu");
+                Variables.SettingsMenu = new Menu("Spells", $"{Variables.MainMenuName}.settingsmenu");
                 {
                     Variables.QMenu = new Menu("Use Q to:", $"{Variables.MainMenuName}.qmenu")
                         .SetFontStyle(FontStyle.Regular, SharpDX.Color.Green);
