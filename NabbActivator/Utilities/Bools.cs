9a13688eacb8031822f66f40adc2762934e170d8
ExoryREPO <drago.enrico@hotmail.it>
2016-01-26 11:36:30 +0100

    NabbActivator: Reworked Bools.

--- NabbActivator/Utilities/Bools.cs
+++ NabbActivator/Utilities/Bools.cs
@@ -15,19 +15,19 @@ namespace NabbActivator
         /// </summary>
         public static bool IsHealthPotRunning()
         =>
-            ObjectManager.Player.HasBuff("ItemMiniRegenPotion") ||
             ObjectManager.Player.HasBuff("ItemCrystalFlask") ||
+            ObjectManager.Player.HasBuff("RegenerationPotion") ||
+            ObjectManager.Player.HasBuff("ItemMiniRegenPotion") ||
             ObjectManager.Player.HasBuff("ItemDarkCrystalFlask") ||
-            ObjectManager.Player.HasBuff("ItemCrystalFlaskJungle") ||
-            ObjectManager.Player.HasBuff("RegenerationPotion");
+            ObjectManager.Player.HasBuff("ItemCrystalFlaskJungle");
 
         /// <summary>
         /// Defines whether a Mana Potion is running.
         /// </summary>
         public static bool IsManaPotRunning()
         =>
-            ObjectManager.Player.HasBuff("ItemCrystalFlaskJungle") ||
-            ObjectManager.Player.HasBuff("ItemDarkCrystalFlask");
+            ObjectManager.Player.HasBuff("ItemDarkCrystalFlask") ||
+            ObjectManager.Player.HasBuff("ItemCrystalFlaskJungle");
 
         /// <summary>
         /// Defines whether a target has no protection.
@@ -42,16 +42,18 @@ namespace NabbActivator
         /// Defines whether the player should use cleanse.
         /// </summary>
         public static bool ShouldUseCleanse(Obj_AI_Hero target)
-        =>  
-            Bools.HasNoProtection(ObjectManager.Player) &&
-            target.HasBuffOfType(BuffType.Charm) ||
-            target.HasBuffOfType(BuffType.Flee) ||
-            target.HasBuffOfType(BuffType.Polymorph) ||
+        =>
             Bools.IsValidSnare() ||
-            target.HasBuffOfType(BuffType.Stun) ||
-            target.HasBuffOfType(BuffType.Taunt) ||
+            target.HasBuff("summonerdot") ||
             target.HasBuff("summonerexhaust") ||
-            target.HasBuff("summonerdot");
+            Bools.HasNoProtection(ObjectManager.Player) &&
+            (
+                target.HasBuffOfType(BuffType.Flee) ||
+                target.HasBuffOfType(BuffType.Stun) ||
+                target.HasBuffOfType(BuffType.Charm) ||
+                target.HasBuffOfType(BuffType.Taunt) ||
+                target.HasBuffOfType(BuffType.Polymorph)
+            );
 
         /// <summary>
         /// Defines whether the player should use a cleanser.
@@ -59,12 +61,14 @@ namespace NabbActivator
         public static bool ShouldUseCleanser()
         =>
             Bools.HasNoProtection(ObjectManager.Player) &&
-            (ObjectManager.Player.HasBuff("zedulttargetmark") ||
-            ObjectManager.Player.HasBuff("VladimirHemoplague") ||
-            ObjectManager.Player.HasBuff("MordekaiserChildrenOfTheGrave") ||
-            ObjectManager.Player.HasBuff("PoppyDiplomaticImmunity") ||
-            ObjectManager.Player.HasBuff("FizzMarinerDoom") ||
-            ObjectManager.Player.HasBuffOfType(BuffType.Suppression));
+            ObjectManager.Player.HasBuffOfType(BuffType.Suppression) &&
+            (
+                ObjectManager.Player.HasBuff("FizzMarinerDoom") ||
+                ObjectManager.Player.HasBuff("zedulttargetmark") ||
+                ObjectManager.Player.HasBuff("VladimirHemoplague") ||
+                ObjectManager.Player.HasBuff("PoppyDiplomaticImmunity") ||
+                ObjectManager.Player.HasBuff("MordekaiserChildrenOfTheGrave")
+            );
 
         /// <summary>
         /// Defines whether the casted root is worth cleansing.
@@ -74,8 +78,11 @@ namespace NabbActivator
             ObjectManager.Player.Buffs
                 .Any(buff =>
                     buff.Type.Equals(BuffType.Snare) &&
-                    !((Obj_AI_Hero)buff.Caster).ChampionName.Equals("Leona") &&
-                    !((Obj_AI_Hero)buff.Caster).ChampionName.Equals("Amumu"));
+                    (
+                        !((Obj_AI_Hero)buff.Caster).ChampionName.Equals("Leona") &&
+                        !((Obj_AI_Hero)buff.Caster).ChampionName.Equals("Amumu")
+                    )
+                );
 
         /// <summary>
         /// Defines whether the arg spell is available and ready.
