937e90f580c84f035f3239d2289bb902608853f3
ExoryREPO <drago.enrico@hotmail.it>
2016-02-01 16:09:40 +0100

    ExorAIO: Darius Blind fixes.

--- ExorAIO/AIO/Champions/Darius/Properties/Logics.cs
+++ ExorAIO/AIO/Champions/Darius/Properties/Logics.cs
@@ -24,10 +24,10 @@ namespace ExorAIO.Champions.Darius
             /// The Q KillSteal Logic.
             /// </summary>
             if (Variables.Q.IsReady() &&
-                !Targets.Target.IsValidTarget(Variables.Q.Range - 220f) &&
                 Targets.Target.IsValidTarget(Variables.Q.Range) &&
+                !Targets.Target.IsValidTarget(Variables.Q.Range - 220f) &&
 
-                ((!Targets.Target.UnderTurret() &&
+                ((!ObjectManager.Player.UnderTurret() &&
 					ObjectManager.Player.ManaPercent >= ManaManager.NeededQMana &&
                     Variables.Menu.Item($"{Variables.MainMenuName}.qspell.harass").GetValue<bool>()) ||
 
@@ -42,8 +42,8 @@ namespace ExorAIO.Champions.Darius
             /// </summary>
             if (Variables.E.IsReady() &&
                 Targets.Target.IsValidTarget(Variables.E.Range) &&
+                Variables.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo &&
                 ObjectManager.Player.Distance(Variables.E.GetPrediction(Targets.Target).UnitPosition) < Variables.E.Range &&
-				Variables.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo &&
                 Variables.Menu.Item($"{Variables.MainMenuName}.espell.combo").GetValue<bool>())
             {
                 Variables.E.Cast(Variables.E.GetPrediction(Targets.Target).UnitPosition);
@@ -54,7 +54,7 @@ namespace ExorAIO.Champions.Darius
             /// </summary>
             if (Variables.R.IsReady() &&
                 Targets.Target.IsValidTarget(Variables.R.Range) &&
-                KillSteal.GetRDamage(Targets.Target) < Targets.Target.Health &&
+                KillSteal.GetRDamage(Targets.Target) > Targets.Target.Health &&
                 Variables.Menu.Item($"{Variables.MainMenuName}.rspell.ks").GetValue<bool>())
             {
                 Variables.R.CastOnUnit(Targets.Target);
@@ -72,8 +72,8 @@ namespace ExorAIO.Champions.Darius
             /// The W Combo Logic.
             /// </summary>
             if (Variables.W.IsReady() &&
-                ((Obj_AI_Hero)args.Target).IsValidTarget(Orbwalking.GetRealAutoAttackRange(Targets.Target)) &&
                 Variables.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo &&
+                ((Obj_AI_Hero)args.Target).IsValidTarget(Orbwalking.GetRealAutoAttackRange(Targets.Target)) &&
                 Variables.Menu.Item($"{Variables.MainMenuName}.wspell.combo").GetValue<bool>())
             {
                 Variables.W.Cast();
@@ -91,9 +91,9 @@ namespace ExorAIO.Champions.Darius
             /// The Q Farm Logic.
             /// </summary>
             if (Variables.Q.IsReady() &&
-                Variables.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear &&
                 ObjectManager.Player.ManaPercent >= ManaManager.NeededQMana &&
-                ((Targets.Minions?.Count() >= 3) ||
+                Variables.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear &&
+                (Targets.Minions?.Count() >= 3 ||
                     GameObjects.Jungle.Contains((Obj_AI_Minion)args.Target)) &&
                 Variables.Menu.Item($"{Variables.MainMenuName}.qspell.farm").GetValue<bool>())
             {
