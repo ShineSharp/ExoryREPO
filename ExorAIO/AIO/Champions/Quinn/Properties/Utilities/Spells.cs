d7bb66677e5c2d39ff85c2482056b56b5073e60f
ExoryREPO <drago.enrico@hotmail.it>
2016-02-03 22:23:51 +0100

    ExorAIO: Quinn - Fixed Spell values.

--- ExorAIO/AIO/Champions/Quinn/Properties/Utilities/Spells.cs
+++ ExorAIO/AIO/Champions/Quinn/Properties/Utilities/Spells.cs
@@ -15,12 +15,12 @@ namespace ExorAIO.Champions.Quinn
         /// </summary>
         public static void Initialize()
         {
-            Variables.Q = new Spell(SpellSlot.Q, 1000f);
+            Variables.Q = new Spell(SpellSlot.Q, 1025f);
             Variables.W = new Spell(SpellSlot.W, 2100f);
-            Variables.E = new Spell(SpellSlot.E, 700f);
+            Variables.E = new Spell(SpellSlot.E, 675f);
             Variables.R = new Spell(SpellSlot.R);
 
-            Variables.Q.SetSkillshot(0.25f, 90f, 1550f, true, SkillshotType.SkillshotLine);
+            Variables.Q.SetSkillshot(0.25f, 210f, 1550f, true, SkillshotType.SkillshotLine);
             Variables.E.SetTargetted(0.25f, 2000f);
         }
     }
