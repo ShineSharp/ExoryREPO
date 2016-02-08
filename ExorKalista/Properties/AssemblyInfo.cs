11af2e08a82f0cf69996f8733a203cadbd26224c
ExoryREPO <drago.enrico@hotmail.it>
2016-02-08 03:08:26 +0100

    ExorKalista: New Description~

--- ExorKalista/Properties/AssemblyInfo.cs
+++ ExorKalista/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExorKalista")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("~Your final destination.~")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LeagueSharp")]
 [assembly: AssemblyProduct("ExorKalista")]
