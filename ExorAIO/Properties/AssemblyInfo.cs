1aa5181719de0b66cacc1ad7172e11079f687022
ExoryREPO <drago.enrico@hotmail.it>
2016-02-08 03:08:34 +0100

    ExorAIO: New Description~

--- ExorAIO/Properties/AssemblyInfo.cs
+++ ExorAIO/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExorAIO")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("~Play til your heart content.~")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LeagueSharp")]
 [assembly: AssemblyProduct("ExorAIO")]
