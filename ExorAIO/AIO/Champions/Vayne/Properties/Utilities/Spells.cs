1dfb95faf74ccde208809c9bb1ff870c41a74712
ExoryREPO <drago.enrico@hotmail.it>
2016-02-09 14:25:38 +0100

    ExorAIO: Vayne - SPrediction Fix.

--- ExorAIO/AIO/Champions/Vayne/Properties/Utilities/Spells.cs
+++ ExorAIO/AIO/Champions/Vayne/Properties/Utilities/Spells.cs
@@ -19,8 +19,8 @@ namespace ExorAIO.Champions.Vayne
             Variables.W = new Spell(SpellSlot.W);
             Variables.E = new Spell(SpellSlot.E, ObjectManager.Player.BoundingRadius*2 + 550f);
 
-            //Variables.E.SetTargetted(0.25f, 1250f);
-            Variables.E.SetTargetted(0.413f, 1000f);
+            //Variables.E.SetTargetted(0.413f, 1000f);
+            Variables.E.SetSkillshot(0.413f, Targets.Target.BoundingRadius, 1000f, false, SkillshotType.SkillshotLine);
         }
     }
 }
